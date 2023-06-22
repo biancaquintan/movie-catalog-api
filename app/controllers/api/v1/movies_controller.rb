a8307ee08a898655ce54d5d54ee5a4a95b59c73e
Bianca <you@example.com>
2023-06-21 23:23:40 -0300

    creates routes

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::MoviesController < ActionController::API
+  # POST /api/v1/movies/import_catalog
+  def import_catalog
+    head 200
+  end
+
+  # GET /api/v1/movies/list_catalog
+  def list_catalog
+    @movies = Movie.all
+    render json: @movies, status: 200
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,8 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      post '/movies/import_catalog', action: :import_catalog, controller: 'movies'
+      get '/movies/list_catalog', action: :list_catalog, controller: 'movies'
+    end
+  end
 end
