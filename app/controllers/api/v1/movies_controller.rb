d92643f519b6fa5d044817900cec0e606c82e057
Bianca <you@example.com>
2023-06-23 00:09:40 -0300

    adds endpoints

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,12 +1,18 @@
 class Api::V1::MoviesController < ActionController::API
   # POST /api/v1/movies/import_catalog
   def import_catalog
-    head 200
+    begin
+      Movie.import
+      render(json: {message: "Catalog imported successfully!"}, status: :ok)
+    rescue => e
+      render(json: { error: e.message }, status: :unprocessable_entity)
+    end
   end
 
   # GET /api/v1/movies/list_catalog
   def list_catalog
     @movies = Movie.all
-    render json: @movies, status: 200
+    render json: @movies.order('year DESC'), status: 200
   end
 end
+
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,20 @@
 class Movie < ApplicationRecord
+  require 'csv'
+
   validates :title, uniqueness: true, presence: true
   validates :description, length: { maximum: 500 }
+
+  def self.import
+    CSV.foreach(("lib/files/netflix_titles.csv"), headers: true) do |row|
+    
+    Movie.create!(
+                    genre: row['type'],
+                    title: row['title'],
+                    country: row['country'],
+                    published_at: row['date_added'],
+                    year: row['release_year'],
+                    description: row['description']
+                  )
+    end
+  end
 end
